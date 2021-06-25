244c610a262fc901570dbd14a1624d4a96eab41c
tslever <46533664+tslever@users.noreply.github.com>
2021-06-25 03:03:49 -0400

    Updated introductory comment and ReadMe
    for Utilities_for_organizing_lines_from_a_CSV_file_into_a_dataframe.
    
    Going to share with Elder Research.

--- Utilities_for_organizing_lines_from_a_CSV_file_into_a_dataframe/Organize_lines_from_a_CSV_file_into_a_dataframe.py
+++ Utilities_for_organizing_lines_from_a_CSV_file_into_a_dataframe/Organize_lines_from_a_CSV_file_into_a_dataframe.py
@@ -7,16 +7,23 @@ from Exceptions.An_Invalid_Path_Exception import *
 from Exceptions.An_Invalid_Line_Indices_Exception import *
 
 
-##############################################################################################################################################################
-# main represents the primary function of the "Organize lines from a CSV file into a dataframe" program, which organizes lines in a CSV file into a dataframe.
-# Usage: python CSV-File_Organizer.py <the path to the CSV file> <the index of the first line to read> <the index of the last line to read>
+############################################################################################################################################################
+# main represents the primary function of the "Organize lines from a CSV file into a dataframe" program, which organizes lines in a CSV file between a first
+# index and a last index into a dataframe.
 #
+# Usage: At command line, enter on one line
+# python Organize_lines_from_a_CSV_file_into_a_dataframe.py
+# <the path to the CSV file> <the index of the first line to read> <the index of the last line to read>
+#
+# Example Usage: At command line, enter on one line
+# python Organize_lines_from_a_CSV_file_into_a_dataframe.py ../the_sibling_folder/the_CSV_file.csv 1000 1002
+
 # Author: Tom Lever
 # Version: 1.0
 # Since: 06/24/21
 #
 # Tom Lever retains the right to maintain the "Organize lines from a CSV file into a dataframe" as an example of Python-based object-oriented programming.
-##############################################################################################################################################################
+###########################################################################################################################################################
 
 def main():
 
